a1fa0827c6a24dc5bbe6a306167aef0731241045
kbub27 <kalebmerriman27@gmail.com>
2019-07-19 19:16:20 -0700

    figuring out heroku

---
+++ client/build/precache-manifest.6feef93d1a19e6e49ffaf29b4760fc2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "eedb4302c2c1857941bb",
+    "url": "/static/js/main.dd1c7d20.chunk.js"
+  },
+  {
+    "revision": "73a92ea81af2477f9057",
+    "url": "/static/js/2.44b3b530.chunk.js"
+  },
+  {
+    "revision": "eedb4302c2c1857941bb",
+    "url": "/static/css/main.ec69924a.chunk.css"
+  },
+  {
+    "revision": "72fe0c2297f87fe8384c7d4cfa75e25c",
+    "url": "/index.html"
+  }
+];
